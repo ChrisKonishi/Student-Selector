6c7fa0e04d01c30a09f7721752d1a21b1a58e060
Chris <christian.konishi@gmail.com>
2020-08-02 12:11:16 -0300

    primeira versao do Student_reader

--- src/Student_reader.py
+++ src/Student_reader.py
@@ -15,4 +15,35 @@ class Student_reader():
     def process_data(self):
         data = pd.read_excel(self.file)
 
+        sub_student = {}
+        for i in self.subjects:
+            sub_student[i] = []
+
+        all_student = []
+
+        for row in range(len(data)):
+            stt_dic = {}
+
+            #aqui eh a atribuicao de dados
+            for att in self.attributes.keys():
+                stt_dic[att] = data[self.attributes[att][row]]
+
+            att["Ordem chegada"] = row + 1
+            #fim da coleta de dados
+
+            student = Student(stt_dic)
+
+            all_student.append(student)
+
+            #decidir materia(s)
+            for i in self.subjects:
+                if i in stt_dic["materia inscrita"]:
+                    sub_student[i].append(student)
+
+
+        return all_student, sub_student
+
+
+        
+
         
