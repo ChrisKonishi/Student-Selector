1ea16985e177e6a2745ddba644e5f30633575721
Chris <christian.konishi@gmail.com>
2020-08-10 23:01:58 -0300

    Approver feito, sem teste

---
+++ src/Approver.py
@@ -0,0 +1,26 @@
+'''
+Decides if a student is allowed to enroll in a subject
+
+init(dict: limit)
+limit: dictionary containing the limit number of students, divided by subject
+
+call(dict: students)
+students: dictionary containing all students sorted, divided by subject
+returns: dictionary containing the students, divided by subject. The students are updated, marking in which subjects they are allowed to enroll
+'''
+class Approver():
+    def __init__(self, limit):
+        self.limit = limit
+
+    def __call__(self, students):
+        for i in self.limit.keys():
+            for j in range(self.limit[i]):
+                
+                try:
+                    students[i][j].approve(i)
+
+                except IndexError:
+                    break
+                
+        return students
+
